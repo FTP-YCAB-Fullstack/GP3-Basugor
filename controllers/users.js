d7a1ccb93ca186a51b9bbaf287f3972fe2289915
PC13-LAB TPM-1 <087762156347.ky@gmail.com>
2021-09-29 13:51:40 +0800

    controller user

--- controllers/users.js
+++ controllers/users.js
@@ -1,2 +1,17 @@
+const userModel = require('../models').user
 
 
+class Users {
+    static login = async (req,res,next)=>{
+        try {
+            const {email,password} = req.body
+            const user = await userModel.findOne({
+                where:{
+                    email:email
+                }
+            })
+        } catch (error) {
+            next(error)
+        }
+    }
+}
