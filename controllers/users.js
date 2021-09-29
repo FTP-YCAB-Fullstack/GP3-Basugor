b8790778486e8762db78309c95bb1860331fcfd5
NeoGlanding <neoglanding@protonmail.com>
2021-09-29 12:22:14 +0700

    user dapat memasukan email

--- controllers/users.js
+++ controllers/users.js
@@ -1,2 +1,16 @@
+const {user} = require('./../models')
 
+class Users {
+    static signup = async (req, res, next) => {
+        try {
+            let {name, email, password} = req.body;
 
+            let exist = await user.findOne({where: {email}})
+            console.log(exist)
+        } catch (err) {
+            console.log(err)
+        }
+    }
+}
+
+module.exports = Users;
