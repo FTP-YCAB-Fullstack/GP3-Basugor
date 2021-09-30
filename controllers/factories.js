a0f71b59c5db5edc9e485b3ff66eaf675b93168b
PC13-LAB TPM-1 <087762156347.ky@gmail.com>
2021-09-30 16:36:39 +0800

    update factories controll and route

--- controllers/factories.js
+++ controllers/factories.js
@@ -1,13 +1,106 @@
+const { factory } = require("../models");
 
 class Factories {
-    static getAll = async (req, res, next) => {
-      try {
-        res.send("Factories");
-      } catch (error) {
-        next(error);
+  static getAll = async (req, res, next) => {
+    try {
+      const factories = await factory.findAll();
+
+      res.status(200).json(factories);
+    } catch (error) {
+      next({
+        code: 500,
+        message: error.message,
+      });
+    }
+  };
+  static getId = async (req, res, next) => {
+    try {
+      let factoriesId = await factory.findByPk(req.params.factoryId);
+
+      if (!factoriesId) {
+        next({
+          code: 404,
+          message: "factory not found",
+        });
+      } else {
+        res.status(200).json(factoriesId);
       }
-    };
-  }
-  
-  module.exports = Factories;
-  
+    } catch (error) {
+      next({
+        code: 500,
+        message: error.message,
+      });
+    }
+  };
+  static post = async (req, res, next) => {
+    try {
+      let { nameFactory, president, headquarter, founded } = req.body;
+
+      if (!nameFactory || !president || !headquarter || !founded) {
+        return next({
+          code: 400,
+          message: "invalid input,check the fields again",
+        });
+      }
+
+      let newFactory = {
+        nameFactory,
+        president,
+        headquarter,
+        founded,
+      };
+
+      let data = await factory.create(newFactory);
+      res.status(201).json(data);
+    } catch (error) {
+      next({
+        code: 500,
+        message: error.message,
+      });
+    }
+  };
+  static patch = async (req, res, next) => {
+    try {
+      let { nameFactory, president, headquarter, founded } = req.body;
+      let { id } = req.params;
+
+      const update = await factory.update(
+        {
+          nameFactory,
+          president,
+          headquarter,
+          founded,
+        },
+        {
+          where: {
+            id,
+          },
+        }
+      );
+      res.status(201).json({
+        message: "update data success",
+      });
+    } catch (error) {
+      next({
+        code: 500,
+        message: error.message,
+      });
+    }
+  };
+  static deleteFactories = async (req, res, next) => {
+    try {
+      let { id } = req.params;
+
+      const deleteFactories = await factory.findByPk(id);
+      deleteFactories.destroy();
+      res.sendStatus(204);
+    } catch (error) {
+      next({
+        code: 500,
+        message: error.message,
+      });
+    }
+  };
+}
+
+module.exports = Factories;
--- router/factoriesRouter.js
+++ router/factoriesRouter.js
@@ -2,5 +2,9 @@ const router = require("express").Router();
 const Controller = require("../controllers/factories");
 
 router.get("/factories", Controller.getAll);
+router.get("/factories/:factoryId", Controller.getId);
+router.post("/factories", Controller.post);
+router.patch("/factories/:id", Controller.patch);
+router.delete("/factories/:id", Controller.deleteFactories);
 
 module.exports = router;
