d02a7fb039799da101cc767046952e4062882f07
PC13-LAB TPM-1 <087762156347.ky@gmail.com>
2021-09-30 16:43:11 +0800

    add auth in factoriesRoute

--- router/factoriesRouter.js
+++ router/factoriesRouter.js
@@ -1,10 +1,11 @@
 const router = require("express").Router();
 const Controller = require("../controllers/factories");
+const auth = require('../middlewares/auth')
 
-router.get("/factories", Controller.getAll);
-router.get("/factories/:factoryId", Controller.getId);
-router.post("/factories", Controller.post);
-router.patch("/factories/:id", Controller.patch);
-router.delete("/factories/:id", Controller.deleteFactories);
+router.get("/factories", auth.Authentication, auth.Authorization(["user", "admin"]),Controller.getAll);
+router.get("/factories/:factoryId",auth.Authentication, auth.Authorization(["user", "admin"]), Controller.getId);
+router.post("/factories", auth.Authentication, auth.Authorization(["admin"]), Controller.post);
+router.patch("/factories/:id", auth.Authentication, auth.Authorization(["admin"]), Controller.patch);
+router.delete("/factories/:id", auth.Authentication, auth.Authorization(["admin"]), Controller.deleteFactories);
 
 module.exports = router;
