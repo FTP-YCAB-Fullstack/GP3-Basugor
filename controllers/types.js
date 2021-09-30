ba30952c6827f4acc7de2381c835f3986dab40c5
GanXx8 <ganda.imasxx@gmail.com>
2021-09-30 20:43:49 +0700

    GetDetail Types

--- controllers/types.js
+++ controllers/types.js
@@ -13,6 +13,23 @@ class Types {
             })
         };
     };
+    static getDetail = async (req, res, next) => {
+        try {
+        let data = await types.findByPk(req.params.id)
+        
+        if(!data) {
+            next({code: 404, message: 'Types Not Found, try search id'})
+        } else {
+        res.status(200).json({
+            data
+          })
+        };
+        } catch (error) {
+            next({ 
+                code: 500, message: error.message
+            })
+        }
+    };
 }
 
 module.exports = Types;
